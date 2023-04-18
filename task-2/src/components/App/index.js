c6d2d33bd68078b1522763917ea43583d36c93e3
hani <hani_11_123@hotmail.com>
2023-04-18 16:45:03 +0100

    task 2 complete with plan

--- task-2/src/components/App/index.js
+++ task-2/src/components/App/index.js
@@ -1,23 +1,42 @@
 import "./app.css";
-
 import Input from "../Input";
 import List from "../List";
-import { useState } from "react";
+import { useReducer } from "react";
+
+// 1. import useReducer 
+ // call useReducer taking in useReducer (reducer and initial state []) giving back state (name) and dispatch DONE.
+// 2. create function
+ // called Reducer that has two parameters name and action.DONE
+ // include Switch that takes in "ADD-NAME" and returns an array of previous list with new name using spread. DONE 
+ // include default that will throw and error.DONE
+// 3. dispatch function called in jsx 
+ // use anonymous function that calls dispatch to add item and includes action as an object. include user input
+
+ function reducer (name, action){
+  switch (action.type){
+  case "ADD-NAME":
+    return [...name, action.payload.name];
+    default: 
+    throw new Error();
+
+ }
+ }
 
 function App() {
-  const [names, setNames] = useState([]);
 
-  function addName(addedName) {
-    const updatedNames = [...names, addedName];
-    setNames(updatedNames);
-  }
+const [name, dispatch] = useReducer(reducer, [])
+
+function addItem(newName) {
+  dispatch({ type: "ADD-NAME", payload: { name: newName } });
+}
+
 
   return (
     <main>
-      <Input addItem={addName} />
-      <List items={names} />
+      <Input addItem ={addItem} />
+      <List items={name} />
     </main>
   );
-}
-
+  }
 export default App;
+
